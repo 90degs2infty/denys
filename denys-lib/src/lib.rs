8ccb0d6cdc82c88823095419ae5da1b610080d4e
90degs2infty <90degs2infty@posteo.org>
2024-04-21 15:47:19 +0200

    Setup basic rust-targeted workflow

--- denys-lib/src/lib.rs
+++ denys-lib/src/lib.rs
@@ -1,3 +1,13 @@
+// SPDX-FileCopyrightText: (C) 2024 90degs2infty
+// SPDX-FileContributor: 90degs2infty <90degs2infty@posteo.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+//! Library to roll off convex polygons inside one another
+
+/// To be deleted
+#[must_use]
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
